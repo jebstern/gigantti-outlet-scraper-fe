5889c052d25e4f68f939fd34eab09b994fcc94ea
Jesper Skand <jesper.skand@gmail.com>
2020-02-14 16:14:32 +0200

    Add styling to page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,56 @@ import React, { useEffect, useState, ChangeEvent } from 'react'
 import './App.css';
 import { Product, createProductCategory } from './types/index'
 import _ from 'lodash'
+import Container from '@material-ui/core/Container';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import TextField from '@material-ui/core/TextField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    table: {
+      minWidth: 650,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    container: {
+      marginTop: '5rem',
+    }
+  }),
+);
 
 const App = () => {
-
+  const classes = useStyles();
   const [data, setCount] = useState(createProductCategory);
   const [category, setCategory] = useState("uusimmat");
   const [filter, setFilter] = useState("");
   const [filtered, setFiltered] = useState(createProductCategory);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => setCategory(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>, child: React.ReactNode) => setCategory(event.target.value);
   const handleFilterChange = (event: ChangeEvent<HTMLInputElement>) => setFilter(event.target.value);
 
 
@@ -37,56 +77,89 @@ const App = () => {
 
   const formatData = () => {
     const output = filtered.products.map((product: Product, index: number) => {
-      return <tr key={index}>
-        <td><a href={product.link} target="_blank" rel="noopener noreferrer">{ product.title }</a></td>
-        <td>{ product.sale }</td>
-        <td>{ product.normal }</td>
-        <td>{ product.percent }</td>
-        <td>{ product.store }</td>
-        <td>{ product.added }</td>
-      </tr>
+      return <TableRow key={index}>
+        <TableCell component="th" scope="row">
+          <a href={product.link} target="_blank" rel="noopener noreferrer">{ product.title }</a>
+        </TableCell>
+        <TableCell align="right">{ product.sale }</TableCell>
+        <TableCell align="right">{ product.normal }</TableCell>
+        <TableCell align="right">{ product.percent }</TableCell>
+        <TableCell align="right">{ product.store }</TableCell>
+        <TableCell align="right">{ product.added }</TableCell>
+      </TableRow>
     })
     return (
-      <div className="table-container">
-        <span className="tag is-dark"> { output.length } riviä </span>
-        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-          <thead>
-            <tr>
-              <th>Nimi</th>
-              <th>Outlet</th>
-              <th>Normi</th>
-              <th>Alennus</th>
-              <th>Kauppa</th>
-              <th>Lisätty</th>
-            </tr>
-          </thead>
-          <tbody>{ output }</tbody>
-        </table>
-      </div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nimi</TableCell>
+              <TableCell align="right">Outlet</TableCell>
+              <TableCell align="right">Normi</TableCell>
+              <TableCell align="right">Alennus</TableCell>
+              <TableCell align="right">Kauppa</TableCell>
+              <TableCell align="right">Lisätty</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {output}
+            <TableRow><TableCell align="right">{ filtered.products.length } tuotetta</TableCell></TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
   }
 
   return (
-    <section className="section">
+    <div className={classes.container}>
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="age-native-simple">Kategoria</InputLabel>
+            <NativeSelect
+              value={category}
+              onChange={handleChange}
+            >
+              <option value="uusimmat">Uusimmat</option>
+              <option value="kuva">Kuva</option>
+              <option value="aani">Ääni</option>
+              <option value="tietokoneet">Tietokoneet</option>
+              <option value="puhelimet">Puhelimet ja kellot</option>
+              <option>Kamera</option>
+              <option>Kodinkoneet</option>
+              <option>Pienkoneet</option>
+              <option>Pelit ja konsolit</option>
+            </NativeSelect>
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              id="outlined-basic"
+              label="Etsi..."
+              variant="outlined"
+              value={filter}
+              onChange={handleFilterChange}
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
+        </Grid>
+        <Grid item xs></Grid>
+      </Grid>
+
+      <br />
+      <br />
+
       <div className="container">
-        <span className="tag">Valitse kategoria</span>
-        <div className="control">
-          <label className="radio"><input type="radio" name="category" value="uusimmat" onChange={handleChange} checked={category === "uusimmat"} /> Uusimmat</label>
-          <label className="radio"><input type="radio" name="category" value="kuva" onChange={handleChange} checked={category === "kuva"} /> Kuva</label>
-          <label className="radio"><input type="radio" name="category" value="aani" onChange={handleChange} checked={category === "aani"} /> Ääni</label>
-          <label className="radio"><input type="radio" name="category" value="tietokoneet" onChange={handleChange} checked={category === "tietokoneet"} /> Tietokoneet</label>
-          <label className="radio"><input type="radio" name="category" value="puhelimet" onChange={handleChange} checked={category === "puhelimet"} /> Puhelimet ja kellot</label>
-          <label className="radio"><input type="radio" name="category" value="kamera" onChange={handleChange} checked={category === "kamera"} /> Kamera</label>
-          <label className="radio"><input type="radio" name="category" value="kodinkoneet" onChange={handleChange} checked={category === "kodinkoneet"} /> Kodinkoneet</label>
-          <label className="radio"><input type="radio" name="category" value="pienkoneet" onChange={handleChange} checked={category === "pienkoneet"} /> Pienkoneet</label>
-          <label className="radio"><input type="radio" name="category" value="pelit" onChange={handleChange} checked={category === "pelit"} /> Pelit ja konsolit</label>
-        </div>
-        <br />
-        <input className="input" type="text" placeholder="Etsi..." value={filter} onChange={handleFilterChange} />
-        <br /><br />
         { formatData() }
       </div>
-  </section>
+
+  </Container>
+  </div>
   )
 }
 
